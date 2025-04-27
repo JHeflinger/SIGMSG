34d35615e5f1958a99bf54a7c11b741a1b35cb06
Jason Heflinger <jasonheflinger@gmail.com>
2025-04-27 15:22:03 -0400

    wip

--- src/main.c
+++ src/main.c
@@ -1,6 +1,15 @@
 #include <easylogger.h>
+#include <easynet.h>
 
 int main(int argc, char** argv) {
     EZ_INFO("Hello!");
+
+    EZ_INIT_NETWORK();
+    ez_Server* server = EZ_GENERATE_SERVER();
+    EZ_OPEN_SERVER(server, 5000);
+    ez_Connection* connection = EZ_SERVER_ACCEPT(server);
+    EZ_INFO("Got a connection!");
+    EZ_CLEAN_NETWORK();
+
     return 0;
 }
